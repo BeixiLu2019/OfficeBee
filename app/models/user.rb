5c9c3e37db6cd1e3ade85f209c3717d12d8e89dc
Beixi Lu <beixi_lu@yahoo.com>
2020-02-25 18:19:52 +0100

    dashbook for my own offices done, user_id.bookings needs to be finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :offices, dependent: :destroy
-  has_many :offices, through: :bookings
   # validates :last_name, presence: true
   # validates :first_name, presence: true
   validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(
-#   first_name: "Anna",
-#   last_name: "Herzog",
-#   email: "anna.herzog@gmail.com",
-#   bio: "Project boss"
+# # User.create!(
+# #   first_name: "Anna",
+# #   last_name: "Herzog",
+# #   email: "anna.herzog@gmail.com",
+# #   bio: "Project boss"
 
-#   )
+# #   )
 
-# User.create!(
-#   first_name: "Philipp",
-#   last_name: "Hamm",
-#   email: "philipp.hamm@gmail.com",
-#   bio: "Developer"
-#   )
+# # User.create!(
+# #   first_name: "Philipp",
+# #   last_name: "Hamm",
+# #   email: "philipp.hamm@gmail.com",
+# #   bio: "Developer"
+# #   )
 
-User.create!(
-  first_name: "Amy",
-  last_name: "Lu",
-  email: "awesome@yahoo.com" ,
-  bio: "smart girl"
+# # User.create!(
+# #   first_name: "Amy",
+# #   last_name: "Lu",
+# #   email: "awesome@yahoo.com" ,
+# #   bio: "smart girl"
+# #   )
+
+# # User.create!(
+# #   first_name: "Ambar" ,
+# #   last_name: "Galan",
+# #   email: "yahoo@yahoo.com",
+# #   bio: "Boss designer"
+# #   )
+
+Office.create!(
+  city: "Berlin",
+  start_date: Date.new(2000,2,3) ,
+  end_date: Date.new(2002,2,3) ,
+  address: "Haus Strasse 23",
+  max_capacity: 7 ,
+  price: 46,
+  description: "Awesome flat"
   )
-
-# User.create!(
-#   first_name: "Ambar" ,
-#   last_name: "Galan",
-#   email: "yahoo@yahoo.com",
-#   bio: "Boss designer"
-#   )
-
-# Office.create!(
-#   city: "Berlin",
-#   start_date: Date.new(2001,2,3) ,
-#   end_date: Date.new(2002,2,3) ,
-#   address: "Ridwer Strasse 23",
-#   max_capacity: 3 ,
-#   price: 23,
-#   description: "Great flat"
-#   )
-
-# Office.create!(
-#   city: "Berlin",
-#   start_date: Date.new(2000,2,3) ,
-#   end_date: Date.new(2002,2,3) ,
-#   address: "Haus Strasse 23",
-#   max_capacity: 7 ,
-#   price: 46,
-#   description: "Awesome flat"
-#   )
