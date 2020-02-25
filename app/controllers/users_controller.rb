c2d52953a49017479f7c692cb37464edcc8cabdb
Beixi Lu <beixi_lu@yahoo.com>
2020-02-25 12:59:26 +0100

    user_edit&update finished, link on homepage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    @user.save
+    redirect_to root_path
+
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:bio, :email, :first_name, :last_name, :password )
+    end
+end
+
+
+
