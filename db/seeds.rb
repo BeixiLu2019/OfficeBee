f006bf786607b845910fe28d2d83e2dc5c03167e
Ambar Galan <ambar.galan@gmail.com>
2020-02-25 17:54:40 +0100

    Created seeds for Offices to have in our database for experimenting/checking

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,126 @@
 require 'faker'
 
-Office.destroy_all if Rails.env.development?
+# Office.destroy_all if Rails.env.development?
 
-
-puts 'Creating 10 fake offices...'
 10.times do
-  office = Office.new(
-    name: Faker::Hipster.sentence(word_count: 3),
-    description: Faker::Hipster.sentences,
-    city: Faker::Address.city
+  Office.create!(
+    name: "Office #{Faker::Hacker.adjective}",
+    description: Faker::Hacker.say_something_smart,
+    city: Faker::Address.city,
     address: Faker::Address.street_address,
     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
     end_date: Faker::Date.forward(days: 23),
     max_capacity: rand(1..30),
-    price: rand(20..100)
+    price: rand(20..100),
+    user_id: 1
   )
-  office.save!
 end
 
-puts 'Finished!'
+#   two = Office.new(
+#     name: Faker::Hipster.sentence(word_count: 3),
+#     description: Faker::Hipster.sentences,
+#     city: Faker::Address.city,
+#     address: Faker::Address.street_address,
+#     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+#     end_date: Faker::Date.forward(days: 23),
+#     max_capacity: rand(1..30),
+#     price: rand(20..100),
+#     # user_id: 1
+#   )
+#   two.save
+
+#   three = Office.new(
+#     name: Faker::Hipster.sentence(word_count: 3),
+#     description: Faker::Hipster.sentences,
+#     city: Faker::Address.city,
+#     address: Faker::Address.street_address,
+#     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+#     end_date: Faker::Date.forward(days: 23),
+#     max_capacity: rand(1..30),
+#     price: rand(20..100),
+#     # user_id: 1
+#   )
+#   three.save
+
+#   four = Office.new(
+#     name: Faker::Hipster.sentence(word_count: 3),
+#     description: Faker::Hipster.sentences,
+#     city: Faker::Address.city,
+#     address: Faker::Address.street_address,
+#     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+#     end_date: Faker::Date.forward(days: 23),
+#     max_capacity: rand(1..30),
+#     price: rand(20..100),
+#     # user_id: 1,
+#   )
+#   four.save
+
+#   five = Office.new(
+#     name: Faker::Hipster.sentence(word_count: 3),
+#     description: Faker::Hipster.sentences,
+#     city: Faker::Address.city,
+#     address: Faker::Address.street_address,
+#     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+#     end_date: Faker::Date.forward(days: 23),
+#     max_capacity: rand(1..30),
+#     price: rand(20..100),
+#     # user_id: 1
+#   )
+#   five.save
+
+
+
+#  userOne = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     bio: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
+#     # office: one
+#     )
+#  userOne.save
+
+#   userTwo = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     bio: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
+#     # office: two
+#     )
+#  userTwo.save
+
+#   userThree = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     bio: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
+#     # office: three
+#     )
+#  userThree.save
+
+#   userFour = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     bio: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
+#     # office: four
+#     )
+#  userFour.save
+
+#   userFive = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     bio: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
+#     # office: five
+#     )
+#  userFive.save
+
+# puts 'Finished!'
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
@@ -38,12 +140,12 @@ puts 'Finished!'
 #   bio: "Developer"
 #   )
 
-User.create!(
-  first_name: "Amy",
-  last_name: "Lu",
-  email: "awesome@yahoo.com" ,
-  bio: "smart girl"
-  )
+# User.create!(
+#   first_name: "Amy",
+#   last_name: "Lu",
+#   email: "awesome@yahoo.com" ,
+#   bio: "smart girl"
+#   )
 
 # User.create!(
 #   first_name: "Ambar" ,
