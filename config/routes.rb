f7a52599e3c4975f8a15950050fed1ddd4cf087f
Ambar Galan <ambar.galan@gmail.com>
2020-02-25 16:12:20 +0100

    Created Office index and show page with associated controller methods AND updated ROUTES and paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :offices
+  resources :offices do
+    resources :bookings, only: [:create, :update, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 
 end
