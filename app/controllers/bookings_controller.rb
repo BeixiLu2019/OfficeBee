2d482af9115a05966112674580f6a144562937f0
Beixi Lu <beixi_lu@yahoo.com>
2020-02-26 13:39:04 +0100

    routes are updated except for User, Booking all method done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,27 @@
 class BookingsController < ApplicationController
-  before_action :set_office, only: [:new, :show, :edit, :create, :destroy]
+  before_action :set_office, only: [:new, :edit, :create]
   def new
     @booking = Booking.new
   end
+
   def create
     @booking = Booking.new(booking_params)
-    @booking.office = @office
+    @booking.office_id = @office.id
+    @booking.user_id = current_user.id
     @booking.save
-    redirect_to dashboard_path
+    redirect_to booking_path(@booking)
     # link needs to be updated to booking#SHOW
   end
-  def show
+
+  def show #confirmation page
+    @booking = Booking.find(params[:id])
     # total price of one booking should be added on show page
   end
 
   def destroy
-
+    booking = Booking.find(params[:id])
+    booking.destroy
+    redirect_to office_path(@booking.office_id)
   end
 
   private
@@ -23,6 +29,6 @@ class BookingsController < ApplicationController
     @office = Office.find(params[:office_id])
   end
   def booking_params
-    params.require(:office).permit(:date, :number_of_guests, :total_price, :office_id, :user_id)
+    params.require(:booking).permit(:date, :number_of_guests, :office_id, :user_id)
   end
 end
