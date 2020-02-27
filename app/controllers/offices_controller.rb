4fbe17495f2bf786fe85e5fdead980c1136af308
Anna Herzog <anna.herzog@ymail.com>
2020-02-27 12:16:50 +0100

    added access to index office and show page before access

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,4 +1,5 @@
 class OfficesController < ApplicationController
+skip_before_action :authenticate_user!, only: [:index, :show]
 before_action :set_office, only: [:show, :edit, :update, :destroy]
 
   def index
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-
+    @offices = Office.all.sample(3)
   end
 
+
 end
+
