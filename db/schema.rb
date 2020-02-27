fc921938c4e26bd09919ebc8b0a04a7cf923e422
Philipp Hamm <philipp.hamm@gmail.com>
2020-02-27 14:51:09 +0100

    controller index correction and deletion of city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_104908) do
+ActiveRecord::Schema.define(version: 2020_02_27_132944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2020_02_26_104908) do
     t.string "name"
     t.float "latitude"
     t.float "longitude"
-    t.string "city"
     t.index ["user_id"], name: "index_offices_on_user_id"
   end
 
