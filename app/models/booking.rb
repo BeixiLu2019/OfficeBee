2f7be772670e56b5271a044698565d7455cced13
Beixi Lu <beixi_lu@yahoo.com>
2020-02-28 09:01:25 +0100

    show page/booking on office show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,8 +7,9 @@ class Booking < ApplicationRecord
    validate :after_booking_capacity
 
    def after_booking_capacity
-     if number_of_guests > office.max_capacity - office.bookings.sum(:number_of_guests)
-       errors.add(:number_of_guests, "Unfortunately, we don't have enough spaces at the moment.")
-     end
+     # if number_of_guests > office.max_capacity - office.bookings.sum(:number_of_guests)
+     #   errors.add(:number_of_guests, "Unfortunately, we don't have enough spaces at the moment.")
+     # end
    end
+   #call self.office_id then find the office with the self.office_id
 end
